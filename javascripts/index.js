fff414a139d37e1ff7d1ae305f47ec896e074bb0
euglazer <eugenestapler@gmail.com>
2015-06-01 10:43:37 +1200

    finish rectangular array

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,17 +1,9 @@
 
 $(window).load(function () {
 
-  function findSumOf(array) {
-    var sum = 0;
-    for (var i = 0; i < array.length; i++) {
-      sum += array[i];
-    }
-    return sum;
-  }
-
-  // var audio = new Audio('./media/just-say-nothing.mp3');
+  var audio = new Audio('./media/just-say-nothing.mp3');
   // var audio = new Audio('./media/house.mp3');
-  var audio = new Audio('./media/concrete.mp3');
+  // var audio = new Audio('./media/concrete.mp3');
   // var audio = new Audio('./media/change.mp3');
 
   $(document).on('keyup', function (e) {
@@ -24,29 +16,13 @@ $(window).load(function () {
     }
   });
 
-
-  var meowcount = 0;
-  var meow = setInterval(function () {
-    meowcount++;
-    if (meowcount % 2 === 0) {
-      $('body').css({
-        "background" : "white"
-      });
-    } else {
-      $('body').css({
-        "background" : "black"
-      });
-    }
-  }, 10);
-
-
   var spectrumLength = 1024 / 2;
   for (var i = 0; i < spectrumLength; i++) {
     $barContainer = $('<div class="bar-container"></div>');
     $bar = $('<div class="bar"></div>');
     $('#spectrum').append($barContainer);
     $barContainer.append($bar);
-    var degrees = ((i + 1) / spectrumLength) * 5000;
+    // var degrees = ((i + 1) / spectrumLength) * 5000;
     // $barContainer.css({
     //   "webkit-transform" : 'rotate(' + degrees + 'deg)'
     // });
@@ -57,7 +33,6 @@ $(window).load(function () {
   var analyser = context.createAnalyser();
   analyser.fftSize = 1024;
   var output = context.destination;
-
   source.connect(analyser);
   analyser.connect(output);
 
@@ -65,12 +40,10 @@ $(window).load(function () {
   var $bars = $('.bar');
   var interval = setInterval(function() {
     analyser.getByteFrequencyData(frequencyData);
-    console.log(findSumOf(frequencyData));
     $bars.each(function (index, bar) {
       var $bar = $(bar);
       var amplitude = frequencyData[index];
       $bar.css({
-        // 'height' : (amplitude / 255) * 100 + '%',
         '-webkit-transform' : 'rotate(' + amplitude   + 'deg)',
         'background' : 'rgb(255,' + amplitude + ',46)'
       });
